a5d03b3d301ff675f65b0e36d08755fee310a0ce
Lebo Madikane <madikanelebohang@gmail.com>
2025-05-29 09:59:17 +0200

    deployment settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ This disables linting from stopping your build
+  },
 };
 
 export default nextConfig;
